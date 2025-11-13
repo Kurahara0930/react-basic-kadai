7c4b79992e1167b1acb04ea02144887fbec6bc6b
Kurahara0930 <kuma.kura193@gmail.com>
2025-11-13 23:46:21 +0900

    upload project

---
+++ react-profile-card/src/ProfileCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function ProfileCard({ name, age, bio }) {
+    return (
+        <div style={{
+            border: '1px solid #ccc',
+            borderRadius: '8px',
+            padding: '16px'
+        }}>
+            <h2>{name}</h2>
+            <p>【年齢】{age}</p>
+            <p>【自己紹介】{bio}</p>
+        </div>
+    )
+}
+
+export default ProfileCard;
